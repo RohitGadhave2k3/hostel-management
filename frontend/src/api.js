50b622368466b4ba4de1a14e703f3960f7f30e1e
Your Namebipingupta051 <you@example.combipinab73200@gmail.com>
2025-11-30 19:44:42 +0530

    Updated frontend AddEditStudent and backend app.py with CRUD routes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -30,8 +30,11 @@
 // }
 
 
-const API_URL =
-  import.meta.env.VITE_API_URL || "http://backend:5000/api";
+// const API_URL =
+//   import.meta.env.VITE_API_URL || "http://backend:5000/api";
+
+// const API_URL = "http://localhost:5000/api";
+const API_URL = "http://127.0.0.1:5000/api";
 
 export async function getStudents(keyword = "") {
   const res = await fetch(`${API_URL}/students?keyword=${keyword}`);
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,22 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   esbuild: {
+//     loader: "jsx",               // <-- IMPORTANT FIX
+//     include: /src\/.*\.js$/,     // allow JSX inside .js files
+//   },
+//   server: {
+//     host: "0.0.0.0",
+//     port: 5173
+//   }
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    loader: "jsx",               // <-- IMPORTANT FIX
-    include: /src\/.*\.js$/,     // allow JSX inside .js files
-  },
-  server: {
-    host: "0.0.0.0",
-    port: 5173
-  }
 });
